977b996422b208f2fa3a18bfdd7a634b061b2ad7
Japhilko <JanPhilipp.kolb@gmail.com>
2021-03-17 19:00:02 +0100

    comparison slope

---
+++ rcode/chartchallenge_day5_comparisons_slope.R
@@ -0,0 +1,47 @@
+#30DaysChartChallenge 
+# Jan-Philipp Kolb
+# Day5 - comparisons - slope
+
+#https://twitter.com/30DayChartChall/status/1370041826976104451/photo/1
+
+
+
+# Multilevel Model
+
+
+# Load packages -----------------------------------------------------------
+
+
+library(ggplot2)
+library(lme4)
+library(visreg)
+
+set.seed(67)
+groups <- floor(runif(1000, min=1, max=7))
+sex <- rep(c("Male", "Female"), times= 500)
+value1 <- runif(1000, min=1, max=10)
+value2 <- runif(1000, min=1, max=100)
+value3 <- runif(1000, min=1, max=200)
+response <- runif(1000, min=1, max=100)
+
+df <- data.frame(groups, sex, response, value1, value2, value3)
+
+model <- lmer(scale(response) ~ scale(value1) + scale(value2) + scale(value3) + factor(sex) + (1|groups), data=df)
+
+visreg(model
+       , "value1" # Variable to plot
+       , cond = list(value2 = 0, value3 = 0, sex = "Female") # Values of the other variables in the model
+       , gg = TRUE # Use ggplot2 for plotting?
+)
+
+
+
+# Links -------------------------------------------------------------------
+
+
+# https://stats.stackexchange.com/questions/33013/what-test-can-i-use-to-compare-slopes-from-two-or-more-regression-models
+
+
+# https://cran.r-project.org/web/packages/visreg/vignettes/quick-start.html
+
+# https://stats.stackexchange.com/questions/271136/plotting-multilevel-multiple-regression
