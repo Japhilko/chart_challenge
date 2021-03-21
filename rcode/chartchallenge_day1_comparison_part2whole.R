746eedc58e762c274330da097dddab5789286352
Japhilko <JanPhilipp.kolb@gmail.com>
2021-03-21 19:27:23 +0100

    day 1

--- rcode/chartchallenge_day1_comparison_part2whole.R
+++ rcode/chartchallenge_day1_comparison_part2whole.R
@@ -3,11 +3,21 @@
 # Day1 - comparison - part to whole
 
 
+library(treemap)
+
+
+setwd("C:/Arbeit/data/")
+
+cdat <- read.csv("owid-covid-data.csv")
+
+
 
 
 # Links -------------------------------------------------------------------
 
 # https://www.tableau.com/about/blog/2018/11/8-ways-bring-powerful-new-comparisons-viz-audiences-set-actions-97207
 
+# https://datavizcatalogue.com/search/part_to_a_whole.html
 
 
+# https://cran.r-project.org/web/packages/treemap/index.html
