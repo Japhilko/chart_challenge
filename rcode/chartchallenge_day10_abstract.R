9175b51b15045c37ef6f6c2f32cf619c38542bfe
Japhilko <JanPhilipp.kolb@gmail.com>
2021-04-12 21:55:23 +0200

    treemap

--- rcode/chartchallenge_day10_abstract.R
+++ rcode/chartchallenge_day10_abstract.R
@@ -2,3 +2,5 @@
 # Jan-Philipp Kolb
 # Sat Apr 10 12:12:02 2021
 # Day10 - distributions - abstract
+
+
--- rcode/chartchallenge_day9_statistics.R
+++ rcode/chartchallenge_day9_statistics.R
@@ -1,3 +1,23 @@
 #30DaysChartChallenge 
 
 # distributions - statistics
+
+# Andrew Ellis
+
+# Boris Mayer
+
+
+library(tidyverse)
+p <- data_frame(x = c(0, 25)) %>% 
+  ggplot(aes(x = x)) 
+
+p + stat_function(fun = dchisq, args = list(df = 2), color = c("#CD6600"), size = 1.5) +
+  stat_function(fun = dchisq, args = list(df = 4), color = c("#32CD32"), size = 1.5) +
+  stat_function(fun = dchisq, args = list(df = 8), color = "blue", size = 1.5, alpha = 0.7) +
+  stat_function(fun = dchisq, args = list(df = 14), color = "red", size = 1.5, alpha = 0.7) +
+  ggtitle("Chi-Square Distributions with dfs = 2, 4, 8, and 14") +
+  xlab("x") +
+  ylab("dchisq(x, c(2, 4, 8, 14))") +
+  theme_classic()
+
+#30DayChartChallenge - distributions - statics - I used #rstats package tidyverse. The code is from https://methodenlehre.github.io/SGSCLM-R-course/. My code for the chart challenge is on https://github.com/Japhilko/chart_challenge
