63a62c674d67715d7bbe5b65dbfb20939bcf6ec3
Japhilko <JanPhilipp.kolb@gmail.com>
2021-04-18 22:31:49 +0200

    connections

--- rcode/chartchallenge_day16_trees.R
+++ rcode/chartchallenge_day16_trees.R
@@ -5,7 +5,7 @@
 
 
 main_path <- "C:/github/chart_challenge/"
-chart_path <- paste0(main_path,"charts")
+chart_path <- paste0(main_path,"charts/")
 
 
 # trees in Berlin
@@ -15,7 +15,7 @@ chart_path <- paste0(main_path,"charts")
 library(tmaptools)
 library(osmplotr)
 
-cit <- "Berlin"
+cit <- "Mannheim"
 
 
 main_path <- "C:/github/chart_challenge/"
@@ -29,15 +29,18 @@ dat_streets <- extract_osm_objects (key="highway", bbox = bbox)
 
 
 map <- osm_basemap(bbox = bbox, bg = c("black"))
+
+map <- add_osm_objects(map, dat_streets, col = rgb(0,0,1,.1))
 map <- add_osm_objects(map, dat_osm, col = c("#006400"))
-map <- add_osm_objects(map, dat_streets, col = "purple")
 
-
-png(paste0(chart_path,"chart_challenge14_space.png"))
+png(paste0(chart_path,"chart_challenge16_trees.png"))
 print_osm_map(map)
 dev.off()
+
+# Day 16 of the #30DayChartChallenge: trees.  I have the trees in Mannheim and I used #openstreetmap key=natural, value=tree and the #rstats packages tmaptools and osmplotr for this. 
 
 
 # Save the data -----------------------------------------------------------
 
-
+save(dat_osm, file=paste0("C:/Arbeit/data/trees_Mannheim.RData"))
+save(dat_streets, file=paste0("C:/Arbeit/data/streets_Mannheim.RData"))
---
+++ rcode/chartchallenge_day17_popculture.R
@@ -0,0 +1,4 @@
+#30DaysChartChallenge 
+# Jan-Philipp Kolb
+# Sun Apr 18 22:22:10 2021
+# relationships - pop culture
